3eda3080339bf165df7c06bf46346203b06f16a6
mitermitek <188499468+mitermitek@users.noreply.github.com>
2026-08-24 17:08:37 +0200

    feat: add supabase integration and environment configurations

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://pkmdbplmvqxsbumolegr.supabase.co',
+  supabasePublishableKey: 'sb_publishable_88_6iMEY06exMS8dQey9Xw_s6_pX7fg',
+};
